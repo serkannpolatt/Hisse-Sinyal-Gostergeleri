1ab908f7233d2a3ac8eb2cb24ec7321d84e39914
Serkan Polat <itsonlydatahustle@gmail.com>
2024-06-24 09:38:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
-import streamlit as st
-import pandas as pd
 import numpy as np
-import ssl
-from urllib import request, error
-import talib as ta
+import pandas as pd
+import pandas_ta as ta
 from tvDatafeed import TvDatafeed, Interval
+import streamlit as st
+import ssl
+from urllib import request
+
 
 # Function to retrieve stock fundamental data
 def Hisse_Temel_Veriler():
     url1 = "https://www.isyatirim.com.tr/tr-tr/analiz/hisse/Sayfalar/Temel-Degerler-Ve-Oranlar.aspx#page-1"
     context = ssl._create_unverified_context()
-    
-    # Error handling and retry logic
-    max_retries = 3
-    for attempt in range(max_retries):
-        try:
-            response = request.urlopen(url1, context=context)
-            url1 = response.read()
-            df = pd.read_html(url1, decimal=',', thousands='.')
-            df1 = df[2]  # Summary table of all stocks
-            return df1
-        except error.URLError as e:
-            if attempt < max_retries - 1:
-                continue
-            else:
-                st.error(f"Failed to retrieve data: {e}")
-                return pd.DataFrame()  # Return empty DataFrame on failure
+    response = request.urlopen(url1, context=context)
+    url1 = response.read()
+    df = pd.read_html(url1, decimal=',', thousands='.')
+    df1 = df[2]  # Summary table of all stocks
+    return df1
+
+tv = TvDatafeed()
 
 # Tillson T3 calculation function
 def TillsonT3(Close, high, low, vf, length):
     ema_first_input = (high + low + 2 * Close) / 4
-    e1 = ta.EMA(ema_first_input, length)
-    e2 = ta.EMA(e1, length)
-    e3 = ta.EMA(e2, length)
-    e4 = ta.EMA(e3, length)
-    e5 = ta.EMA(e4, length)
-    e6 = ta.EMA(e5, length)
+    e1 = ta.ema(ema_first_input, length)
+    e2 = ta.ema(e1, length)
+    e3 = ta.ema(e2, length)
+    e4 = ta.ema(e3, length)
+    e5 = ta.ema(e4, length)
+    e6 = ta.ema(e5, length)
 
     c1 = -1 * vf * vf * vf
     c2 = 3 * vf * vf + 3 * vf * vf * vf
@@ -88,14 +80,13 @@ def OTT(df, prt, prc):
 
 # Function to generate indicator signals
 def indicator_Signals(Hisse_Adı, Lenght_1, vf, prt, prc):
-    tv = TvDatafeed()
     data = tv.get_hist(symbol=Hisse_Adı, exchange='BIST', interval=Interval.in_daily, n_bars=500)
     data.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close', 'volume': 'Volume'}, inplace=True)
 
     OTT_Signal = OTT(data.copy(deep=True), prt, prc)
     Tillson = TillsonT3(data['Close'], data['High'], data['Low'], vf, Lenght_1)
-    Zscore = ta.ZSCORE(data['Close'], 21)
-    Zsma = ta.SMA(Zscore, 21)
+    Zscore = ta.zscore(data['Close'], 21, 1)
+    Zsma = ta.sma(Zscore)
 
     data['OTT'] = OTT_Signal['OTT3']
     data['Var'] = OTT_Signal['Var']
@@ -114,6 +105,9 @@ def indicator_Signals(Hisse_Adı, Lenght_1, vf, prt, prc):
 
     return data
 
+
+base="light"
+
 st.set_page_config(
     page_title="Hisse Sinyalleri",
     layout="wide",
@@ -122,27 +116,25 @@ st.set_page_config(
 
 with st.sidebar:
     Hisse_Ozet = Hisse_Temel_Veriler()
-    if not Hisse_Ozet.empty:
-        st.header('Hisse Arama')
-        Hisse_Adı = st.selectbox('Hisse Adı', Hisse_Ozet['Kod'])
-        Lenght_1 = 6
-        vf = 0.8
-        prt = 2
-        prc = 1.2
-        data = indicator_Signals(Hisse_Adı, Lenght_1, vf, prt, prc)
+    st.header('Hisse Arama')
+    Hisse_Adı = st.selectbox('Hisse Adı', Hisse_Ozet['Kod'])
+    Lenght_1 = 6
+    vf = 0.8
+    prt = 2
+    prc = 1.2
+    data = indicator_Signals(Hisse_Adı, Lenght_1, vf, prt, prc)
 
-        Son_Durum = data.tail(1)
-        col1, col2, col3, col4, col5 = st.columns(5)
-        Close = Son_Durum['Close'].iloc[0]
-        OTT_Signal = 'Alınabilir' if Son_Durum['OTT_Signal'].iloc[0] else 'Bekle'
-        Zscore_Signal = 'Alınabilir' if Son_Durum['Zscore_Signal'].iloc[0] else 'Bekle'
-        Tillson_Signal = 'Satılabilir' if Son_Durum['Exit'].iloc[0] else 'Bekle'
+Son_Durum = data.tail(1)
+col1, col2, col3, col4, col5 = st.columns(5)
+Close = Son_Durum['Close'].iloc[0]
+OTT_Signal = 'Alınabilir' if Son_Durum['OTT_Signal'].iloc[0] else 'Bekle'
+Zscore_Signal = 'Alınabilir' if Son_Durum['Zscore_Signal'].iloc[0] else 'Bekle'
+Tillson_Signal = 'Satılabilir' if Son_Durum['Exit'].iloc[0] else 'Bekle'
 
-        col2.metric('Kapanış Fiyatı', str(Close))
-        col3.metric('OTT Sinyal', str(OTT_Signal))
-        col4.metric('Z Skor Sinyal', str(Zscore_Signal))
-        col5.metric('Tillson Sinyal', str(Tillson_Signal))
+col2.metric('Kapanış Fiyatı', str(Close))
+col3.metric('OTT Sinyal', str(OTT_Signal))
+col4.metric('Z Skor Sinyal', str(Zscore_Signal))
+col5.metric('Tillson Sinyal', str(Tillson_Signal))
 
-        st.dataframe(data.iloc[::-1], use_container_width=True)
-    else:
-        st.error("Unable to fetch stock data.")
+st.dataframe(data.iloc[::-1], use_container_width=True)
+
